b51e97f1fd87893edc65d3cdb95b99269deb0f74
blue-slushy9 <slushy@localhost.localdomain>
2023-07-20 22:27:43 -0700

    This is a cheat sheet that I decided to turn into a script. I am trying to complete an Arch install and I don't want to have to look up the commands over and over again.

---
+++ Arch1_install.sh
@@ -0,0 +1,22 @@
+# This is for an Arch Linux VM running on KVM on Debian 12;
+
+# View your network interfaces (the VM, not the host system, which has
+# 1 ethernet and 1 wireless interface);
+
+ip link
+
+# Verify your internet connection;
+
+ping archlinux.org
+
+# View time settings;
+
+timedatectl
+
+# Set timezone to PST;
+
+timedatectl set-timezone America/Los_Angeles
+
+# 
+
+
